eaf4d7447aca49b5d9ce6e4aa9152545701f2b0c
AngeloGalav <tekken342@gmail.com>
2022-07-01 15:17:46 +0200

    Add code setup for camera rotations

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -35,7 +35,12 @@ void keyboardPressedEvent(unsigned char key, int x, int y)
 	case 's':
 		mainCamera.moveCameraBack();
 		break;
-	case 'j':
+	// case 'j':
+	// 	mainCamera.forceRotation(-1);
+	// 	break;
+	// case 'k':
+	// 	mainCamera.forceRotation(1);
+	// 	break;
 
 
 	default:
@@ -53,21 +58,29 @@ void mouse(int button, int state, int x, int y)
 	//selected_obj = -1;
 	float closest_intersection = 0.0f;
     selected_obj = -1;
-	for (int i = 0; i < Scena.size(); i++)
-	{
-		float t_dist = 0.0f;
+	// for (int i = 0; i < Scena.size(); i++)
+	// {
+	// 	float t_dist = 0.0f;
 
-		if (raycast.ray_sphere(mainCamera.ViewSetup.position, ray_wor, centri[i], raggi[i], &t_dist))
-		{
-			if (selected_obj == -1 || t_dist < closest_intersection)
-			{
-				selected_obj = i;
-				closest_intersection = t_dist;
-			}
-		}
-	}
+	// 	if (raycast.ray_sphere(mainCamera.ViewSetup.position, ray_wor, centri[i], raggi[i], &t_dist))
+	// 	{
+	// 		if (selected_obj == -1 || t_dist < closest_intersection)
+	// 		{
+	// 			selected_obj = i;
+	// 			closest_intersection = t_dist;
+	// 		}
+	// 	}
+	// }
 
-	printf("Oggetto selezionato %d \n", selected_obj);
+	// stato per controllare che il bottone destro del mouse sia stato effettivamente cliccato
+	if (button == GLUT_RIGHT_BUTTON)
+    {
+		glutSetCursor(GLUT_CURSOR_CROSSHAIR);
+        rotateCamera(x, y);
+    }
+
+
+	// printf("Oggetto selezionato %d \n", selected_obj);
 }
 
 
@@ -78,42 +91,8 @@ void mouse(int button, int state, int x, int y)
  */
 void my_passive_mouse(int xpos, int ypos) // camera rotation function
 {
-	float alfa = 0.05; // serve ridimensionare l'offset tra due posizioni successive del mosue
-	ypos = height - ypos;
-	if (firstMouse)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
-
-	float xoffset = xpos - lastX;
-	float yoffset = ypos - lastY;
-	lastX = xpos;
-	lastY = ypos;
-
-	xoffset *= alfa;
-	yoffset *= alfa;
-	yaw_ += xoffset;   // aggiorno l'angolo yaw
-	pitch_ += yoffset; // aggiorno l'angolo Pitch
-
-	// Facciamo si' che l'angolo di Picht vari tra -90 e 90.
-	if (pitch_ > 89.0f)
-		pitch_ = 89.0f;
-	if (pitch_ < -89.0f)
-		pitch_ = -89.0f;
-
-	glm::vec3 front;
-	front.x = cos(glm::radians(yaw_)) * cos(glm::radians(pitch_));
-	front.y = sin(glm::radians(pitch_));
-	front.z = sin(glm::radians(yaw_)) * cos(glm::radians(pitch_));
-	mainCamera.ViewSetup.direction = vec4(normalize(front), 0.0);
-	mainCamera.ViewSetup.target = mainCamera.ViewSetup.position + mainCamera.ViewSetup.direction;
-
-}
-
-// ruota la camera in base al valore
-void rotateView(float speed, int y, int x){
+	
+	// glutSetCursor(GLUT_CURSOR_INHERIT);
 	// float alfa = 0.05; // serve ridimensionare l'offset tra due posizioni successive del mosue
 	// ypos = height - ypos;
 	// if (firstMouse)
@@ -122,6 +101,8 @@ void rotateView(float speed, int y, int x){
 	// 	lastY = ypos;
 	// 	firstMouse = false;
 	// }
+
+	// // inserire codice per lo scan della velocità del mouse qui
 
 	// float xoffset = xpos - lastX;
 	// float yoffset = ypos - lastY;
@@ -145,4 +126,61 @@ void rotateView(float speed, int y, int x){
 	// front.z = sin(glm::radians(yaw_)) * cos(glm::radians(pitch_));
 	// mainCamera.ViewSetup.direction = vec4(normalize(front), 0.0);
 	// mainCamera.ViewSetup.target = mainCamera.ViewSetup.position + mainCamera.ViewSetup.direction;
+
+}
+
+// funzione che ruota la telecamera durante il gioco
+void rotateCamera(int xpos, int ypos) {
+	float alfa = 0.05; // serve ridimensionare l'offset tra due posizioni successive del mosue
+	ypos = height - ypos;
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	// inserire codice per lo scan della velocità del mouse qui
+
+	float xoffset = xpos - lastX;
+	float yoffset = ypos - lastY;
+	lastX = xpos;
+	lastY = ypos;
+
+	xoffset *= alfa;
+	yoffset *= alfa;
+	yaw_ += xoffset;   // aggiorno l'angolo yaw
+	pitch_ += yoffset; // aggiorno l'angolo Pitch
+
+	// Facciamo si' che l'angolo di Picht vari tra -90 e 90.
+	if (pitch_ > 89.0f)
+		pitch_ = 89.0f;
+	if (pitch_ < -89.0f)
+		pitch_ = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw_)) * cos(glm::radians(pitch_));
+	front.y = sin(glm::radians(pitch_));
+	front.z = sin(glm::radians(yaw_)) * cos(glm::radians(pitch_));
+	mainCamera.ViewSetup.direction = vec4(normalize(front), 0.0);
+	mainCamera.ViewSetup.target = mainCamera.ViewSetup.position + mainCamera.ViewSetup.direction;
+}
+
+void onMouseEvent(int button, int state, int x, int y)
+{
+   if(state == GLUT_DOWN){
+      if(button == GLUT_RIGHT_BUTTON)
+         glutMotionFunc(onRightMouseEvent);
+      else if(button == GLUT_LEFT_BUTTON)
+         glutMotionFunc(onLeftMouseEvent);
+   }
+}
+
+void onLeftMouseEvent(int x, int y){
+	rotateCamera(x, y);
+}
+
+void onRightMouseEvent(int x , int y){
+	// evento per selezionare un cubo(?)
+	// evento action?
 }
--- src/include/InputHandler.h
+++ src/include/InputHandler.h
@@ -7,6 +7,15 @@
 void keyboardPressedEvent(unsigned char key, int x, int y);
 void mouse(int button, int state, int x, int y);
 void my_passive_mouse(int xpos, int ypos);
+void rotateCamera(int xpos, int ypos);
 
+// handles mouse pressure
+void onMouseEvent(int button, int state, int x, int y);
+
+// handles leftMouse event
+void onLeftMouseEvent(int x, int y);
+
+// handles rightMouse event
+void onRightMouseEvent(int x, int y);
 #endif
  
